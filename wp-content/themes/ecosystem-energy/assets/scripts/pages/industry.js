287ca29a077c2823dbbd00d0f1e470f93479e15b
Julien Brousseau <jbrousseau@kryzalid.net>
2022-03-09 10:07:08 -0500

    fix industries

--- wp-content/themes/ecosystem-energy/assets/scripts/pages/industry.js
+++ wp-content/themes/ecosystem-energy/assets/scripts/pages/industry.js
@@ -2,11 +2,11 @@ import Swiper, { Navigation } from 'swiper';
 
 Swiper.use([Navigation]);
 
-const container = document.querySelector('.slider').querySelector('.swiper-container');
+const container = document.querySelector('.industry-cs-slider').querySelector('.swiper-container');
 const slider = new Swiper(container, {
     speed: 300,
     loop: false,
-    slidesPerView: 5,
+    slidesPerView: 4,
     spaceBetween: 30,
     navigation: { nextEl: `.next`, prevEl: `.prev` }
 });
