84bc422ad41f6e77ba44d47aa9e89a3834b03077
Julien Brousseau <jbrousseau@kryzalid.net>
2022-03-10 16:05:23 -0500

    fix responsive home hero

--- wp-content/themes/ecosystem-energy/assets/scripts/pages/front-page.js
+++ wp-content/themes/ecosystem-energy/assets/scripts/pages/front-page.js
@@ -6,12 +6,20 @@ const container = document.querySelector('.home-slider').querySelector('.swiper-
 const slider = new Swiper(container, {
     speed: 300,
     loop: false,
-    slidesPerView: 4,
-    spaceBetween: 30,
-    navigation: { nextEl: `.next`, prevEl: `.prev` }
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: { nextEl: `.next`, prevEl: `.prev` },
+    breakpoints: {
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+    }
 });
-
-
 
 const spinner = document.getElementById('home-spinner');
 
