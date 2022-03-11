2c89e4ec61dca08dba711c55cc85973ae0e76c4f
Julien Brousseau <jbrousseau@kryzalid.net>
2022-03-11 09:36:45 -0500

    add duplicate plugin

--- wp-content/themes/ecosystem-energy/assets/scripts/pages/contact.js
+++ wp-content/themes/ecosystem-energy/assets/scripts/pages/contact.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.office-link').forEach(link => {
   link.addEventListener('click', e => {
-    const content = e.target.nextElementSibling.innerHTML;
-    document.getElementById('office-info').innerHTML = content;
+    // const content = e.target.nextElementSibling.innerHTML;
+    // document.getElementById('office-info').innerHTML = content;
   })
 });
--- wp-content/themes/ecosystem-energy/dist/scripts/pages/contact.js
+++ wp-content/themes/ecosystem-energy/dist/scripts/pages/contact.js
@@ -15,7 +15,7 @@
   \*****************************************/
 /***/ (function() {
 
-eval("document.querySelectorAll('.office-link').forEach(link => {\n  link.addEventListener('click', e => {\n    const content = e.target.nextElementSibling.innerHTML;\n    document.getElementById('office-info').innerHTML = content;\n  })\n});\n\n//# sourceURL=webpack://ecosystem/./assets/scripts/pages/contact.js?");
+eval("document.querySelectorAll('.office-link').forEach(link => {\n  link.addEventListener('click', e => {\n    // const content = e.target.nextElementSibling.innerHTML;\n    // document.getElementById('office-info').innerHTML = content;\n  })\n});\n\n//# sourceURL=webpack://ecosystem/./assets/scripts/pages/contact.js?");
 
 /***/ })
 
