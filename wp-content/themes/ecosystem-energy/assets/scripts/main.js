c868f0b79d732bb01355fc46abe0008f69cb6d56
Julien Brousseau <jbrousseau@kryzalid.net>
2022-03-15 14:14:52 -0400

    fix default page

--- wp-content/themes/ecosystem-energy/assets/scripts/main.js
+++ wp-content/themes/ecosystem-energy/assets/scripts/main.js
@@ -4,9 +4,13 @@ import './utils/aos';
 import './utils/jsBlockLink';
 // Custom gutenberg blocks
 import './utils/custom-blocks';
-// 
+// Initialize contact-cta spinner animation
 import './utils/cta-spinner';
 
 // Initialize header, which manages search field, country toggler and menu 
 import Header from './header/header';
 const header = new Header();
+
+// Change WPML selector tag
+const wpmlSelector = document.querySelector('.wpml-ls-native');
+if (wpmlSelector) wpmlSelector.innerHTML = wpmlSelector.innerHTML.slice(0, 2).toUpperCase();
