88f77e231c4fae8a94c8838efe55485423c3e84d
Julien Brousseau <jbrousseau@kryzalid.net>
2022-03-17 11:10:52 -0400

    fix contact form target

--- wp-content/themes/ecosystem-energy/assets/scripts/pages/contact.js
+++ wp-content/themes/ecosystem-energy/assets/scripts/pages/contact.js
@@ -2,5 +2,8 @@ document.querySelectorAll('.office-link').forEach(link => {
   link.addEventListener('click', e => {
     const content = e.target.nextElementSibling.innerHTML;
     document.getElementById('office-info').innerHTML = content;
+
+    const option = document.querySelector(`.office-field option[value="${ e.target.text }"]`);
+    if (option) option.selected = true;
   })
 });
