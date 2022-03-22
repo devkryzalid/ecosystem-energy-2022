66166f379bbb735ab44a6000ed7448e24b4cf65c
Julien Brousseau <jbrousseau@kryzalid.net>
2022-03-22 15:38:59 -0400

    fix menu issues

--- wp-content/themes/ecosystem-energy/assets/scripts/header/menu.js
+++ wp-content/themes/ecosystem-energy/assets/scripts/header/menu.js
@@ -6,6 +6,7 @@ export default class Menu {
   body = document.body;
   container = document.getElementById('menu');
   button = document.getElementById('menu-toggle');
+  image = document.getElementById('menu-image');
   primaryMenu = document.getElementById('primary-menu');
   
   visible = false;
@@ -17,7 +18,7 @@ export default class Menu {
     
     // Add click listener to burger/close menu button and image
     this.button.addEventListener('click', () => this.toggle());
-    this.container.querySelector('#menu-image').addEventListener('click', () => this.toggle());
+    this.image.addEventListener('click', () => this.toggle());
 
     // Add click listener to all secondary menu triggers
     document.querySelectorAll('.secondary-link').forEach(link => {
@@ -49,37 +50,49 @@ export default class Menu {
     else this.closeMainMenu();
   }
 
-  // Show primary menu
+  // Start menu opening animation
   openMainMenu = () => {
     this.transition = true;
-    this.container.classList.add('show');
-    this.primaryMenu.addEventListener('transitionend', this.onMenuComplete);
+    this.body.classList.add('menu-opening');
+    this.image.addEventListener('transitionend', this.onMenuComplete);
+  }
+
+  // Menu opening animation complete
+  onMenuComplete = () => {
+    this.open = true;
     this.body.classList.add('menu-open');
+    this.transition = false;
+    this.body.classList.remove('menu-opening');
+    this.image.removeEventListener('transitionend', this.onMenuComplete);
   }
 
   // Hide primary menu
   closeMainMenu = () => {
+    this.open = false;
     this.transition = true;
-    this.container.classList.remove('show');
-    this.container.addEventListener('transitionend', this.onMenuClosed);
     this.body.classList.remove('menu-open');
-  }
-
-  // Switch on opened status (on transition end)
-  onMenuComplete = () => {
-    this.open = true;
-    this.transition = false;
-    this.container.classList.add('-on');
-    this.primaryMenu.removeEventListener('transitionend', this.onMenuComplete);
+    this.body.classList.add('menu-closing');
+    this.container.addEventListener('transitionend', this.onMenuClosed);
   }
 
   // Switch on closed status (on transition end)
   onMenuClosed = () => {
-    this.open = false;
     this.transition = false;
-    this.container.classList.remove('-on');
+    this.body.classList.remove('menu-closing');
     this.container.removeEventListener('transitionend', this.onMenuClosed);
   }
+
+  // toggleMenu = open => {
+  //   this.open = !!open;
+  //   if (this.open) this.body.classList.add('menu-open');
+  //   else this.body.classList.remove('menu-open');
+  // }
+
+  // toggleTransition = enable => {
+  //   this.transition = !!enable;
+  //   if (this.transition) this.body.classList.add('menu-transition');
+  //   else this.body.classList.remove('menu-transition');
+  // }
 
   // Secondary menu controls
   openSecondaryMenu = event => {
