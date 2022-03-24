a3d912f6a4173157f497a511f0c7d829dc154389
Julien Brousseau <jbrousseau@kryzalid.net>
2022-03-24 11:18:53 -0400

    add selected state on contact offices

--- wp-content/themes/ecosystem-energy/assets/scripts/pages/contact.js
+++ wp-content/themes/ecosystem-energy/assets/scripts/pages/contact.js
@@ -1,9 +1,17 @@
 document.querySelectorAll('.office-link').forEach(link => {
-  link.addEventListener('click', e => {
-    const content = e.target.nextElementSibling.innerHTML;
+  link.addEventListener('click', ({ target }) => {
+
+    // Toggle selected state
+    document.querySelectorAll('.office-link.selected').forEach(el => el.classList.remove('selected'));
+    target.classList.add('selected');
+
+    // Change office-info content
+    const content = target.nextElementSibling.innerHTML;
     document.getElementById('office-info').innerHTML = content;
 
-    const option = document.querySelector(`.office-field option[value="${ e.target.text }"]`);
+    // Select office in form
+    const option = document.querySelector(`.office-field option[value="${ target.text }"]`);
     if (option) option.selected = true;
+
   })
 });
