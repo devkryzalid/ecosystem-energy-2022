2c7bc3e6e8416edff2605f8cbf6e18329444b4d4
Julien Brousseau <jbrousseau@kryzalid.net>
2022-03-28 11:29:03 -0400

    improve leadership page

--- wp-content/themes/ecosystem-energy/assets/scripts/pages/leadership.js
+++ wp-content/themes/ecosystem-energy/assets/scripts/pages/leadership.js
@@ -4,7 +4,7 @@ const sidePanel = new SidePanel({ containerId: 'panel-body', onDataLoaded: id =>
 
 // Set data-id for prev/next buttons
 function setNav (id) {
-  const allIds = [...document.querySelectorAll('.leader-content')].map(el => el.dataset.id);
+  const allIds = [...document.querySelectorAll('.leadership-details')].map(el => el.dataset.id);
   const index = allIds.indexOf(id);
   document.getElementById('panel-prev').setAttribute('data-id', allIds[index - 1] || allIds.length);
   document.getElementById('panel-next').setAttribute('data-id', allIds[index + 1] || allIds[0]);
